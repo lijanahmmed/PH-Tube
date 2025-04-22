2f5dd5d7802aacc225ce4740af4b2109479816f2
lijanahmmed <lijanahmmed@gmail.com>
2025-04-22 22:56:59 +0600

    ph tube without change

--- js/index.js
+++ js/index.js
@@ -10,27 +10,6 @@ const loadVideos = () => {
     .then(data => displayVideos(data.videos))
 }
 
-// authors
-// : 
-// [{…}]
-// category_id
-// : 
-// "1001"
-// description
-// : 
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// : 
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// : 
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// : 
-// "Shape of You"
-// video_id
-// : 
-// "aaaa"
 
 const displayVideos = (videos) => {
     const videosContainer = document.getElementById("videos")
