7d047a84bcd1f9eebec47fd1112c7119e35364e4
lijanahmmed <lijanahmmed@gmail.com>
2025-04-24 22:50:43 +0600

    ph tube videos category

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+function getTimeString (time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`
+}
+
 const loadCategories = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then(res => res.json())
@@ -10,47 +18,35 @@ const loadVideos = () => {
     .then(data => displayVideos(data.videos))
 }
 
-
-// {category_id: '1001', video_id: 'aaaa', thumbnail: 'https://i.ibb.co/L1b6xSq/shape.jpg', title: 'Shape of You', authors: Array(1), …}
-// authors
-// : 
-// [{…}]
-// category_id
-// : 
-// "1001"
-// description
-// : 
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// : 
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// : 
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// : 
-// "Shape of You"
-// video_id
-// : 
-// "aaaa"
+const loadCategoryVideos = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => displayVideos(data.category))
+}
 
 const displayVideos = (videos) => {
     const videosContainer = document.getElementById("videos")
     videos.forEach((video) => {
-        console.log(video)
         const card = document.createElement("div")
         card.classList = "card"
         card.innerHTML = `
-        <figure>
-            <img
+        <figure class="h-[200px] relative">
+            <img class="h-full w-full object-cover"
             src=${video.thumbnail}
             alt="Shoes" />
+            ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black p-1 text-white text-xs">${getTimeString(video.others.posted_date)}</span>`}
         </figure>
-        <div class="card-body">
-            <h2 class="card-title">${video.title}</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+        <div class="px-0 py-2 flex gap-3">
+            <div>
+                <img class="h-10 w-10 rounded-full object-cover" src= ${video.authors[0].profile_picture} />
+            </div>
+            <div>
+                <h2 class="text-xl font-bold">${video.title}</h2>
+                <div class="flex gap-1 items-center">
+                    <p class="text-gray-400">${video.authors[0].profile_name}</p>
+                    ${video.authors[0].verified === true ? `<img class="h-4 w-4 rounded-full object-cover" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />` : ""}
+                </div>
+                <p class="text-sm text-gray-400">${video.others.views} views</>
             </div>
          </div>
         `
@@ -61,10 +57,11 @@ const displayVideos = (videos) => {
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById("categories")
     categories.forEach((item) => {     
-    const button = document.createElement("button");
-    button.classList = "btn btn-primary";
-    button.innerText = item.category;
-    categoryContainer.append(button)    
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `
+    <button onclick="loadCategoryVideos(${item.category_id})" class="btn">${item.category}</button>
+    `
+    categoryContainer.append(buttonContainer)    
     });
 }
 
